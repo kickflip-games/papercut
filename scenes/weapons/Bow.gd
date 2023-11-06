bf98940f635a1c953d78b3e5344b2d33019222bc
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2023-11-06 09:53:42 +0530

    add deathmahine

--- scenes/weapons/Bow.gd
+++ scenes/weapons/Bow.gd
@@ -1,21 +1,39 @@
 extends Weapon
 
-@onready var shootPoint:Marker2D = $Marker2D
+@onready var shoot_point:Marker2D = $Marker2D
 
 
 var arrow:PackedScene = preload("res://scenes/weapons/arrow.tscn")
 
+@export_range(0.001,50) var shot_arc_lim:float
 
+func _init():
+	cooldown = 0.01
+
+
+func _get_rand_angle():
+	var angle = randf_range(-shot_arc_lim, shot_arc_lim)
+	print("angle: ", angle)
+	return deg_to_rad(angle)
+
+func _get_shot_dir():
+	var dir = global_position.direction_to(shoot_point.global_position)
+	var shot_dir = dir.rotated(_get_rand_angle())
+	return shot_dir
+	
 
 func shoot():
 	
 	if not attack_enabled:
 		return
 	
-	var arrow_instance = arrow.instantiate()
+	var shot_dir = _get_shot_dir()
+	var arrow_instance = arrow.instantiate() 
+	
+	arrow_instance.global_transform = shoot_point.global_transform
 	get_tree().get_root().call_deferred("add_child", arrow_instance)
-	arrow_instance.global_transform = shootPoint.global_transform
-	arrow_instance.direction_vector = global_position.direction_to(shootPoint.global_position)
+	arrow_instance.speed = 500
+	arrow_instance.direction_vector = shot_dir
 	cooldown_timer.start(cooldown)
 	enable_attack(false)
 	
@@ -26,3 +44,18 @@ func _physics_process(delta):
 		shoot()
 
 
+func _draw():
+	draw_line(
+		Vector2(),
+		shoot_point.position,
+		Color.GREEN,
+		5
+	)
+	draw_arc(
+		Vector2.ZERO,
+		position.distance_to(shoot_point.position) * 0.1,
+		-deg_to_rad(shot_arc_lim) + PI/2,
+		deg_to_rad(shot_arc_lim) + PI/2,
+		10,
+		Color.GREEN
+	)
